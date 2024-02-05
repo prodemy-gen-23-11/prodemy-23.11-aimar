fbcfb4e01ea04539e6af6c6225e4d83b0a3cc0ea
Aimar Ristiansyah <aimarristiansyah23@gmail.com>
2024-02-06 00:28:42 +0700

    update size img

--- gen-19-react-component/product-card/src/components/productCard.jsx
+++ gen-19-react-component/product-card/src/components/productCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function productCard({ name, price, image, sold, onBuyClick }) {
   return (
     <div className="bg-gray-800 p-4 rounded-lg shadow-lg">
-      <img src={image} alt={name} className="w-44 h-auto rounded-lg " />
+      <img src={image} alt={name} className="w-44 h-auto rounded-lg" />
       <h3 className="text-xl font-semibold text-white mt-2">{name}</h3>
       <p className="text-white">Rp. {price}</p>
       <p className="text-gray-400">{sold}</p>
