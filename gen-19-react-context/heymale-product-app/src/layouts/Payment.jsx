9746ef363da6b23183ad8c2494a72bfc00add36c
Aimar Ristiansyah <aimarristiansyah23@gmail.com>
2024-02-27 21:34:25 +0700

    Updated context

--- gen-19-react-context/heymale-product-app/src/layouts/Payment.jsx
+++ gen-19-react-context/heymale-product-app/src/layouts/Payment.jsx
@@ -1,42 +1,40 @@
 import React, { useContext } from "react";
 import { CoContext } from "../context/CoContext";
+import toRupiah from "../util/formatter";
 
 function Payment() {
   const { dataCo } = useContext(CoContext);
   console.log(dataCo);
 
-  // const navigate = useNavigate();
-
   return (
-    <section className="px-20">
-      <h1 className="my-6 text-3xl font-semibold">Payment</h1>
-      <div>
+    <div>
+      {dataCo.length === 0 ? (
+        <p>Bag is empty!</p>
+      ) : (
         <div>
-          <p className="text-2xl">Your Bag</p>
-          <hr />
-          <div className="flex flex-col">
-            <div className="flex justify-between my-4">
-              <img
-                src={dataCo?.image?.[3]}
-                alt={dataCo.name}
-                className="w-16"
-              />
-              <p className="font-semibold">{dataCo.name}</p>
-              <span>{dataCo.qty}</span>
-              <span>{dataCo.price}</span>
+          {dataCo.map((item) => (
+            <div key={item.id}>
+              <hr />
+              <div className="flex flex-col">
+                <div className="flex justify-between my-4">
+                  <img src={item.image?.[3]} alt={item.name} className="w-16" />
+                  <p className="font-semibold">{item.name}</p>
+                  <span>{item.qty}</span>
+                  <span>{toRupiah(item.price)}</span>
+                </div>
+              </div>
+              <hr />
+              <div className="flex justify-between mt-4">
+                <span className="font-bold">Total</span>
+                <span className="font-bold">
+                  {toRupiah(item.qty && item.price ? item.qty * item.price : 0)}
+                </span>
+              </div>
             </div>
-          </div>
-          <hr />
-
-          <div className="flex justify-between mt-4">
-            <span className="font-bold">Total</span>
-            <span className="font-bold">
-              {dataCo.qty && dataCo.price ? dataCo.qty * dataCo.price : 0}
-            </span>
-          </div>
+          ))}
         </div>
-      </div>
-    </section>
+      )}
+    </div>
   );
 }
 
